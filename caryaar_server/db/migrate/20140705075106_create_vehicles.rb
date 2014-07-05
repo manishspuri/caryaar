09ef8cfb932d828a3afc4c40a334b6aaaa93a578
Ankur Gera <ankur_gera@external.mckinsey.com>
2014-07-05 13:49:05 +0530

    Migrations for Vehicle and Registration :smile:

--- caryaar_server/db/migrate/20140705075106_create_vehicles.rb
+++ caryaar_server/db/migrate/20140705075106_create_vehicles.rb
@@ -1,8 +1,22 @@
 class CreateVehicles < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :vehicles do |t|
-
+      t.string :type
+      t.string :brand
+      t.integer :no_of_available_seats
+      t.string :city
+      t.string :from_location
+      t.string :to_location
+      t.string :owner_name
+      t.string :owner_phone
+      t.string :owner_email
+      t.date :travel_date
+      t.time :travel_time
       t.timestamps
     end
+  end
+
+  def self.down
+    drop_table :vehicles
   end
 end
