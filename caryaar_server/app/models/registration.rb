4920bb9f0fd0d81e46ccb6e16cc85d27f36a2f8c
Manish Puri <manishspuri@gmail.com>
2014-07-05 17:08:31 +0530

    mailer added :envelope:

--- caryaar_server/app/models/registration.rb
+++ caryaar_server/app/models/registration.rb
@@ -10,7 +10,5 @@ class Registration < ActiveRecord::Base
   	self.vehicle.deduct_seat_by_one
   end
 
-  def send_email
-  	Vehicle.report_email
-  end
+
 end
--- caryaar_server/app/models/vehicle.rb
+++ caryaar_server/app/models/vehicle.rb
@@ -9,6 +9,13 @@ class Vehicle < ActiveRecord::Base
   scope :seats_available, -> { where(["no_of_available_seats > 0"])}
   scope :future_dates, -> {where(["travel_date >= ? ", Date.today])}
 
+  before_save :send_email
+
+
+  def send_email
+    Notifier.send_email_alert(@vehicle).deliver!
+  end
+
   def deduct_seat_by_one
   	if self.no_of_available_seats > 0 
   	  self.no_of_available_seats -= 1  
