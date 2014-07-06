cba2dcb18b13cc0131dbd89e08d74fba93a957bc
Vaibhav kohli <vaibhav.khl@gmail.com>
2014-07-07 00:42:02 +0530

    code formatting

--- caryar_client/www/scripts/controllers/home_controller.js
+++ caryar_client/www/scripts/controllers/home_controller.js
@@ -7,41 +7,41 @@ angular.module('carYaarApp')
 		// $scope.confvenue = SetData.get_venue_data();
 
 		HomeService.get()
-			    .success(function(data){
-			    	$scope.cars = data;
-			    })
+	    .success(function(data){
+	    	$scope.cars = data;
+	    })
     
-	    $scope.registerVehicle = function() {
-	    	HomeService.registerVehicle($scope.vehicle)
-				.success(function(data) {
+    $scope.registerVehicle = function() {
+    	HomeService.registerVehicle($scope.vehicle)
+			  .success(function(data) {
 					HomeService.get()
 				    .success(function(data){
 				    	$scope.cars = data;
 				    	$location.path('/');
 				    })
-			    })
-			    .error(function(err){
-			    	console.log(err);
-			    });
+		    })
+		    .error(function(err){
+		    	console.log(err);
+		    });
 		};
 
 		$scope.requestVehicle = function(vehicleId) {
 		  $rootScope.vehicleId = vehicleId;
-          $location.path('/request/' + vehicleId);
+      $location.path('/request/' + vehicleId);
 		};
 
 		$scope.requestThisVehicle = function() {
 		  $scope.request.vehicle_id = $rootScope.vehicleId;
-          HomeService.requestVehicle($scope.request)
-                .success(function(data) {
-					HomeService.get()
-				    .success(function(data){
+      HomeService.requestVehicle($scope.request)
+        .success(function(data) {
+			    HomeService.get()
+		        .success(function(data){
 				    	$scope.cars = data;
 				    	$location.path('/');
-				    })
-			    })
-			    .error(function(err){
-			    	console.log(err);
-			    });
+			      })
+	      })
+		    .error(function(err){
+		    	console.log(err);
+		    });
 		};   
 	});
